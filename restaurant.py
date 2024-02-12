df72ba085b2e95fc00f68e6f9d4c97f26385e193
cjacob2 <144711668+cjacob2@users.noreply.github.com>
2024-02-12 10:10:09 -0500

    Add files via upload

--- restaurant.py
+++ restaurant.py
@@ -1,50 +1,78 @@
 #CS175
 #Conor Jacob
 #restaurant
-def run_again():
-    again = input("Would you like to run again?(Yes/No) ").lower()
-    if again == "yes":
-        run = True
-    else:
-        exit()
+      
 run = True
-while run:
-    veg = input("Is anyone in your party vegetarian?(Yes/No) ").lower()
-    vegan = input("Is anyone in your party vegan?(Yes/No) ").lower()
-    gluten = input("Is anyone in your party gluten-free?(Yes/No) ").lower()
+while run == True:
+
+    inp = True
+    while inp:
+        veg = input("Is anyone in your party vegetarian?(Yes/No) ").lower()
+        if veg == "yes" or veg == "no":
+            inp = False
+        else:
+            print("Incorrect input. Enter Yes/No")
+            
+    inp = True
+    while inp:
+        vegan = input("Is anyone in your party vegan?(Yes/No) ").lower()
+        if vegan == "yes" or vegan == "no":
+            inp = False
+        else:
+            print("Incorrect input. Enter Yes/No")
+            
+    inp = True
+    while inp:
+        gluten = input("Is anyone in your party gluten-free?(Yes/No) ").lower()
+        if gluten == "yes" or gluten == "no":
+            inp = False
+        else:
+            print("Incorrect input. Enter Yes/No")
+
     if veg == "no" and vegan == "no" and gluten == "no":
-        print("Here are your restaurant choices." +
-              "\nJoe's Gourmet Burgers" +
-              "\nMama's Fine Italian" +
-              "\nMain Street Pizza Company"+
-              "\nCorner Cafe"+
-              "\nThe Chef's Kitchen")
-        run_again()
-    elif veg == "yes" and vegan == "no" and gluten == "no":
-        print("Here are your restaurant choices." +
-              "\nMama's Fine Italian" +
-              "\nMain Street Pizza Company"+
-              "\nCorner Cafe"+
-              "\nThe Chef's Kitchen")
-        run_again()
-    elif veg == "no" and vegan == "no" and gluten == "yes":
-        print("Here are your restaurant choices." +
-              "\nMain Street Pizza Company"+
-              "\nCorner Cafe"+
-              "\nThe Chef's Kitchen")
-        run_again()
-    elif veg == "yes" and vegan == "no" and gluten == "yes":
-        print("Here are your restaurant choices." +
-              "\nMain Street Pizza Company"+
-              "\nCorner Cafe"+
-              "\nThe Chef's Kitchen")
-        run_again()
-    else:
-        print("Here are your restaurant choices." +
-              "\nCorner Cafe"+
-              "\nThe Chef's Kitchen")
-        run_again()
+        print("Here are your restaurant choices."
+                "\nJoe's Gourmet Burgers" 
+                "\nMama's Fine Italian" 
+                "\nMain Street Pizza Company"
+                "\nCorner Cafe"
+                "\nThe Chef's Kitchen")
         
+    elif veg == "yes" and vegan == "no" and gluten == "no":
+        print("Here are your restaurant choices." 
+              "\nMama's Fine Italian" 
+              "\nMain Street Pizza Company"
+              "\nCorner Cafe"
+              "\nThe Chef's Kitchen")
+            
+    elif veg == "no" and vegan == "no" and gluten == "yes":
+        print("Here are your restaurant choices."
+              "\nMain Street Pizza Company"
+              "\nCorner Cafe"
+              "\nThe Chef's Kitchen")
+            
+    elif veg == "yes" and vegan == "no" and gluten == "yes":
+        print("Here are your restaurant choices." 
+              "\nMain Street Pizza Company"
+              "\nCorner Cafe"
+              "\nThe Chef's Kitchen")
+              
+    else:
+        print("Here are your restaurant choices."
+              "\nCorner Cafe"
+              "\nThe Chef's Kitchen")
+    inp = True
+    while inp== True:
+        again = input("Would you like to run again?(Yes to run again/No to exit program) ").lower()
+        if again == "yes":
+            inp = False
+        elif again == "no":
+            print("Exiting program")
+            inp = False
+            run = False
+        else:
+            print("Incorrect input. Enter Yes/No")
+
+    
     
 
         
