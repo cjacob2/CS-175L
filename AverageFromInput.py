338543e885d3c63054d065be51e5d6edd0c9499a
cjacob2 <144711668+cjacob2@users.noreply.github.com>
2024-03-19 21:50:31 -0400

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -2,19 +2,35 @@
 #Conor Jacob
 #AverageFromInput
 
-
 def main():
-    total_num = 0
-    count_nums = 0 
-    with open(r"C:\Users\cdjac\Downloads\numbers.txt", "r") as file:
-        for line in file:
-            num = float(line.strip())
-            total_num += num
-            count_nums +=1
-            print(f"I read {count_nums} number(s) Current number is : {num:9.2f} Total is : {total_num:9.2f}")
+    try:
+        
+        total_num = 0
+        count_nums = 0
+        
+        with open(r"C:\Users\cdjac\Downloads\numbers.txt", "r") as file:
+            for line in file:
+                try:
+            
+                    num = float(line.strip())
+                    total_num += num
+                    count_nums +=1
+                    print(f"I read {count_nums} number(s) Current number is : {num:9.2f} Total is : {total_num:9.2f}")
 
+                except ValueError:
+                    print(f"Bad Data: {line.strip()} skipping")
+
+        avr(total_num, count_nums)
+        
+    except FileNotFoundError:
+        print("SystemExit: File not found: numbers.txt - exiting")
+
+    
+
+
+def avr(total_num, count_nums):
     avr = total_num/count_nums if count_nums>0 else 0
     print(f"Average: {avr:.1f}")
 
-if  1 == 1 :
-    main()
+
+main()
